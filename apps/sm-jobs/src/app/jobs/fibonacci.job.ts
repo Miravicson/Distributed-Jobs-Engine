71d1dc2cb7a358f7f08ed9a03cda2c5aa3054dc6
Miravicson <victorughonu@gmail.com>
2025-05-04 19:08:58 +0100

    implemented jobs module, jobs decorator and wired up job module dependencies.

---
+++ apps/sm-jobs/src/app/jobs/fibonacci.job.ts
@@ -0,0 +1,8 @@
+import { Job } from '../decorators/job.decorators';
+import { AbstractJob } from './abstract-job';
+
+@Job({
+  name: 'Fibonacci',
+  description: 'Generate a Fibonnacci sequence and store it in the DB.',
+})
+export class FibonacciJob extends AbstractJob {}
