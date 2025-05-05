6764a723f9e039ff0c09c418fe342fe23aeadcbb
Miravicson <victorughonu@gmail.com>
2025-05-05 13:38:07 +0100

    feat(jobs): refactor Fibonacci job and consumer; add validation for job data

--- apps/executor/src/app/jobs/fibonacci/fibonacci.consumer.ts
+++ apps/executor/src/app/jobs/fibonacci/fibonacci.consumer.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PulsarClient, PulsarConsumer } from '@sm/pulsar';
 import { FibonacciData } from './fibonacci-data.interface';
 import { iterate } from 'fibonacci';
@@ -12,9 +12,6 @@ export class FibonacciConsumer
     super(pulsarClient, 'Fibonacci');
   }
   protected async onMessage(data: FibonacciData): Promise<void> {
-    if (!data.iterations) {
-      throw new BadRequestException('Invalid data');
-    }
     const result = iterate(data.iterations);
     this.logger.log(`Fibonnaci result: ${JSON.stringify(result)}`);
   }
--- apps/sm-jobs/src/app/jobs/fibonacci/fibonacci.job.ts
+++ apps/sm-jobs/src/app/jobs/fibonacci/fibonacci.job.ts
@@ -1,13 +1,14 @@
 import { PulsarClient } from '@sm/pulsar';
 import { Job } from '../../decorators/job.decorators';
 import { AbstractJob } from '../abstract-job';
-import { FibonacciData } from './fibonacci-data.interface';
+import { FibonacciData } from './fibonacci-data.message';
 
 @Job({
   name: 'Fibonacci',
   description: 'Generate a Fibonnacci sequence and store it in the DB.',
 })
 export class FibonacciJob extends AbstractJob<FibonacciData> {
+  protected messageClass = FibonacciData;
   constructor(pulsarClient: PulsarClient) {
     super(pulsarClient);
   }
