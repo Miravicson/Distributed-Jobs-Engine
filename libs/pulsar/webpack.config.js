cca62564286ce12e3bf46360790ca4c1b9de0c8f
Miravicson <victorughonu@gmail.com>
2025-05-06 16:04:59 +0100

    feat: add webpack configuration files for app and library
    - Created `webpack.app.config.js` for application bundling with NxAppWebpackPlugin.
    - Created `webpack.lib.config.js` for library bundling with NxAppWebpackPlugin.
    - Configured TypeScript compilation, asset handling, and source maps in both configurations.

--- libs/pulsar/webpack.config.js
+++ libs/pulsar/webpack.config.js
@@ -1,19 +1,29 @@
-const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
-const { join } = require('path');
+// const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
+// const { join } = require('path');
 
-module.exports = {
+// module.exports = {
+//   output: {
+//     path: join(__dirname, '../../dist/libs/pulsar'),
+//   },
+//   plugins: [
+//     new NxAppWebpackPlugin({
+//       target: 'node',
+//       compiler: 'tsc',
+//       main: './src/index.ts',
+//       tsConfig: './tsconfig.lib.json',
+//       optimization: false,
+//       outputHashing: 'none',
+//       generatePackageJson: true,
+//     }),
+//   ],
+// };
+
+const { join } = require('path');
+const { merge } = require('webpack-merge');
+const commonConfig = require('../../webpack.lib.config');
+
+module.exports = merge(commonConfig, {
   output: {
     path: join(__dirname, '../../dist/libs/pulsar'),
   },
-  plugins: [
-    new NxAppWebpackPlugin({
-      target: 'node',
-      compiler: 'tsc',
-      main: './src/index.ts',
-      tsConfig: './tsconfig.lib.json',
-      optimization: false,
-      outputHashing: 'none',
-      generatePackageJson: true,
-    }),
-  ],
-};
+});
