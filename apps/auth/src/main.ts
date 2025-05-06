bd9324caeba28ac9acaa9f60ff8af9a7fc69ed69
Miravicson <victorughonu@gmail.com>
2025-05-06 07:22:08 +0100

    fix(init): modify init function to conditionally start server and log application URL

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -4,10 +4,12 @@ import { GrpcOptions, Transport } from '@nestjs/microservices';
 import { AUTH_PACKAGE_NAME } from '@sm/grpc';
 import { join } from 'path';
 import { init } from '@sm/nestjs';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await init(app);
+  await init(app, false);
   app.connectMicroservice<GrpcOptions>({
     transport: Transport.GRPC,
     options: {
@@ -15,7 +17,15 @@ async function bootstrap() {
       protoPath: join(__dirname, '../../libs/grpc/proto/auth.proto'),
     },
   });
+  // Start microservices first
   await app.startAllMicroservices();
+
+  // THEN start HTTP server
+  const configService = app.get(ConfigService);
+  const port = configService.getOrThrow('PORT');
+  await app.listen(port);
+
+  Logger.log(`🚀 Application is running on: http://localhost:${port}/api`);
 }
 
 bootstrap();
--- libs/nestjs/src/lib/init.ts
+++ libs/nestjs/src/lib/init.ts
@@ -2,15 +2,19 @@ import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
 
-export async function init(app: INestApplication) {
+export async function init(app: INestApplication, startServer = true) {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.use(cookieParser());
 
-  const port = app.get(ConfigService).getOrThrow('PORT');
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  if (startServer) {
+    const port = app.get(ConfigService).getOrThrow('PORT');
+    await app.listen(port);
+    Logger.log(
+      `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    );
+  }
+
+  return app;
 }
